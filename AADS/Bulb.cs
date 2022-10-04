192e46fdb6906e8127ac0255915e7ad25c7d5767
Beray <bsezgin00@gmail.com>
2022-10-04 23:21:57 +0300

    Test commit

---
+++ AADS/Bulb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AADS {
+    internal class Bulb {
+
+        // Code
+        //int[] states = { 0, 1, 0, 1 };
+        //int N = states.Length;
+
+        // Function Code
+int bulbs(int[] A, int N) {
+
+            // To keep track of switch presses so far
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                if (A[i] == 0) {
+                    A[i] = 1;
+                    for (int j = i + 1; j < N; j++) {
+                        if (A[j] == 1) {
+                            A[j] = 0;
+                        } else {
+                            A[j] = 1;
+                        }
+                    }
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}

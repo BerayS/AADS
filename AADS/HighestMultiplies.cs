c67a2fb06d3206043447d1f761784f3728f5a8bb
Beray <bsezgin00@gmail.com>
2022-10-18 21:30:36 +0300

    Testing commit

--- AADS/HighestMultiplies.cs
+++ AADS/HighestMultiplies.cs
@@ -49,5 +49,24 @@ namespace AADS {
 
         //Console.WriteLine(finalResult);
         //    }
+//        using System.Linq;
+//// Highest Product
+//// Test Cases:
+//// {1, 2, 3, 4} - 24 ; {0, -1, 10, 5, 7} - 350
+//// {-5, -2, -1, 0, 0, 1, 1, 5} - 50; {-5, -2, -1, 0, 0, 3, 4, 5} - 60;
+//List<int> n = new List<int> { 0, -1, 10, 5, 7 };
+
+//    n.Sort();
+
+//Console.WriteLine(String.Join(", ",n));
+
+//int lo2hi1 = n[0] * n[1] * n[n.Count - 1];
+//    int hi3 = n[n.Count - 1] * n[n.Count - 2] * n[n.Count - 3];
+
+//if(lo2hi1 > hi3) {
+//    Console.WriteLine(lo2hi1);
+//} else {
+//    Console.WriteLine(hi3);
+//}
     }
 }
--- AADS/Program.cs
+++ AADS/Program.cs
@@ -1,19 +1,71 @@
-﻿using System.Linq;
-// Highest Product
-// Test Cases:
-// {1, 2, 3, 4} - 24 ; {0, -1, 10, 5, 7} - 350
-// {-5, -2, -1, 0, 0, 1, 1, 5} - 50; {-5, -2, -1, 0, 0, 3, 4, 5} - 60;
-List<int> n = new List<int> { 0, -1, 10, 5, 7 };
+﻿//// Largest Permutation
 
-n.Sort();
+//using System.Collections.Immutable;
 
-Console.WriteLine(String.Join(", ",n));
+//string[] tokens = Console.ReadLine().Split(' ');
+//string[] arr = Console.ReadLine().Split(' ');
+//int n = int.Parse(tokens[0]);
+//int k = int.Parse(tokens[1]);
 
-int lo2hi1 = n[0] * n[1] * n[n.Count - 1];
-int hi3 = n[n.Count - 1] * n[n.Count - 2] * n[n.Count - 3];
+//if (tokens.Length != 2 || arr.Length != n) {
+//    throw new ArgumentException("Invalid input");
+//}
 
-if(lo2hi1 > hi3) {
-    Console.WriteLine(lo2hi1);
-} else {
-    Console.WriteLine(hi3);
+//for (int i = 0; i < k; i++) {
+//    int tempVar;
+//    int maks;
+//    if (arr[0] == arr.Max()) {
+//        continue;
+//    } else {
+//        arr[0] = arr.Max();
+//    }
+
+//    maks = int.Parse(arr[0].Max());
+
+//}
+
+//int[] arr = new int[5] {4, 2, 3, 5, 1};
+
+//Console.WriteLine(LargestPermutation(arr, 1));
+
+//string LargestPermutation(int[] arr,int k) {
+//    int tempVar;
+//    foreach (int num in arr) {
+//        if (arr[0] == arr.Max()) {
+//            continue;
+//        } else {
+//            tempVar = arr[0];
+//        }
+//    }
+
+//    string asd = string.Join(", ", arr);
+
+//    return asd;
+//}
+
+string[] s = Console.ReadLine().Split(' ');
+int N = int.Parse(s[0]);
+int K = int.Parse(s[1]);
+s = Console.ReadLine().Split(' ');
+int[] P = new int[N];
+int[] V = new int[N];
+
+for (int i = 0; i < N; ++i) {
+    int val = int.Parse(s[i]) - 1;
+    P[val] = i;
+    V[i] = val;
 }
+for (int i = 0; i < N && K > 0; ++i)
+    if (P[N - i - 1] != i) {
+        int idx = P[N - i - 1];
+        int val = V[i];
+        P[N - i - 1] = i;
+        V[i] = N - i - 1;
+        P[val] = idx;
+        V[idx] = val;
+        K--;
+    }
+Console.Write(V[0] + 1);
+for (int i = 1; i < N; ++i)
+    Console.Write(" {0}", V[i] + 1);
+Console.WriteLine();
